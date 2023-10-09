142a4bb4ba190ecc1c9912999f7a095f765251fc
Aurelien lebray <aurelien.lebray@gmail.com>
2023-10-09 19:34:33 +0200

    Lesson 09/10/2023

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BookReview, BookReviewTicket
 
 # Register your models here.
+admin.site.register(BookReview)
+admin.site.register(BookReviewTicket)
